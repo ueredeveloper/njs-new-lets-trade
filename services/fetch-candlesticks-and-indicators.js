0e8c9993796bd1e8562a11470b8b4890bddfbb38
Fabrício José Barrozo <fabricio.barrozo@adasa.df.gov.br>
2025-05-12 14:18:43 -0300

    feat: adiciona filtro de stablecoins

--- services/fetch-candlesticks-and-indicators.js
+++ services/fetch-candlesticks-and-indicators.js
@@ -88,3 +88,56 @@ router.get("/candlesticks-and-indicators", async (req, res) => {
 });
 
 module.exports = router;
+
+/*
+{
+    "symbol": "BTCUSDT",
+    "interval": "1h",
+    "price": "104006.85000000",
+    "candlesticks": {
+        "values": [
+            {
+                "openTime": 1746154800000,
+                "open": "96952.59000000",
+                "high": "97233.10000000",
+                "low": "96952.58000000",
+                "close": "97102.32000000",
+                "volume": "434.57258000",
+                "closeTime": 1746158399999,
+                "quoteVolume": "42204257.76606470",
+                "trades": 85999,
+                "baseAssetVolume": "215.02117000",
+                "quoteAssetVolume": "20881016.69513660"
+            },
+            ...
+            
+            {
+                "openTime": 1747051200000,
+                "open": "103762.89000000",
+                "high": "104098.94000000",
+                "low": "103729.04000000",
+                "close": "104006.85000000",
+                "volume": "1026.14629000",
+                "closeTime": 1747054799999,
+                "quoteVolume": "106642512.54434330",
+                "trades": 136062,
+                "baseAssetVolume": "640.50878000",
+                "quoteAssetVolume": "66561247.80617060"
+            }
+        ]
+    },
+    "indicators": [
+        {
+            "type": "sma20",
+            "period": 20,
+            "symbol": "BTCUSDT",
+            "interval": "1h",
+            "values": [
+                96957.082,
+                96946.32299999999,
+                96934.35249999998,
+               
+            ],
+            ...
+
+*/
